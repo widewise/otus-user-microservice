5862c2a5ba114fd1404e3a07d09af2c6966e5c69
Aleksandr Mulgachev <a.mulgachev@urent.ru>
2023-06-14 13:04:13 +0400

    cr fix.

--- run.bat
+++ run.bat
@@ -1,10 +1,8 @@
-kubectl create namespace m
+kubectl create namespace otus
 
-helm install nginx ingress-nginx/ingress-nginx --namespace m -f https://cdn.otus.ru/media/public/20/14/nginx_ingress-25239-20146a.yaml
+helm install nginx ingress-nginx/ingress-nginx --namespace otus -f https://cdn.otus.ru/media/public/20/14/nginx_ingress-25239-20146a.yaml
 
-kubectl create secret generic secret-appsettings  --namespace m --from-file=./Otus.Microservice.User/appsettings.secrets.json
-kubectl create secret generic postgres-secrets --namespace m \
---from-literal=POSTGRES_USER="postgres" \
---from-literal=POSTGRES_PASSWORD="postgres"
+kubectl create secret generic secret-appsettings  --namespace otus --from-file=./Otus.Microservice.User/appsettings.secrets.json
+kubectl create secret generic postgres-secrets --namespace otus --from-literal=POSTGRES_USER="postgres" --from-literal=POSTGRES_PASSWORD="postgres"
 
 kubectl apply -f manifest.yml

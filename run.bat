007b685e79ed2a8adbb0142a28326a992fdca89d
Aleksandr Mulgachev <a.mulgachev@urent.ru>
2023-06-15 21:11:20 +0400

    helm install postgresql

--- run.bat
+++ run.bat
@@ -2,7 +2,8 @@ kubectl create namespace otus
 
 helm install nginx ingress-nginx/ingress-nginx --namespace otus -f https://cdn.otus.ru/media/public/20/14/nginx_ingress-25239-20146a.yaml
 
+helm install postgres --namespace otus bitnami/postgresql --namespace otus -f values.yml
+
 kubectl create secret generic secret-appsettings  --namespace otus --from-file=./Otus.Microservice.User/appsettings.secrets.json
-kubectl create secret generic postgres-secrets --namespace otus --from-literal=POSTGRES_USER="postgres" --from-literal=POSTGRES_PASSWORD="postgres"
 
 kubectl apply -f manifest.yml
